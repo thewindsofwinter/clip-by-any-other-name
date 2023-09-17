3cf06509628b8277ca8dfcdf238ea7b5d869f06a
Andy Tang <winterwind2022@gmail.com>
2023-09-17 02:54:12 -0400

    no package conflicts

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ import pkg_resources
 from setuptools import setup, find_packages
 
 setup(
-    name="clip",
-    py_modules=["clip"],
+    name="clip-by-any-other-name",
+    py_modules=["clip-by-any-other-name"],
     version="1.0",
     description="",
     author="OpenAI",
